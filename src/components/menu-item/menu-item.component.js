325ba1755dad48c90600f028d4d8950df3415a9d
Aram Mikayelian <aram@Arams-MacBook-Pro.local>
2019-07-30 13:45:56 -0700

    built components and styled menu-item components

---
+++ src/components/menu-item/menu-item.component.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../menu-item/menu-item.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  //destructuring props to use our title prop
+  //passed from directory component
+  //string interpelation {`${}'}to bring in our size properties and then style
+  //allows us much more flexibility to dynamicly build and style our components
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
+    {/* No closing div tag, we didn't wrap our content with background-image div so our content can retain size */}
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      {/* can use JS here to use function touppercase since it's jsx */}
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
